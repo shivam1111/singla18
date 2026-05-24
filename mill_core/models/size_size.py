9811775b0e6e7b36f68d5a888245c37fbc268657
shivamgoyal <shivam1111@gmail.com>
2026-05-24 23:41:31 +0530

    Added edge to size name

--- mill_core/models/size_size.py
+++ mill_core/models/size_size.py
@@ -25,7 +25,7 @@ class Size(models.Model):
         store=True,index=True
     )
 
-    corner_id = fields.Many2one('corner.type',name = "Corner Type")
+    corner_id = fields.Many2one('corner.type',name = "Corner Type",required=True)
     width_mm = fields.Float("Width (mm)", digits=(10, 2))
     thickness_mm = fields.Float("Thickness (mm)", digits=(10, 2))
     diameter_mm = fields.Float("Diameter (mm)", digits=(10, 2))
@@ -50,7 +50,7 @@ class Size(models.Model):
             if rec.shape == 'round' and rec.diameter_mm:
                 rec.name = f"Ø {rec.diameter_mm:g}"
             elif rec.shape == 'flat' and rec.width_mm and rec.thickness_mm:
-                rec.name = f"{rec.width_mm:g}x{rec.thickness_mm:g}"
+                rec.name = f"{rec.width_mm:g}x{rec.thickness_mm:g}({rec.corner_id.name})"
             else:
                 rec.name = "Undefined Size"
 
