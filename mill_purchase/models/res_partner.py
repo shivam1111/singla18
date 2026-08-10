45e2ad5e8098466e4739d44f6ff4b4eb36e6015f
shivamgoyal <shivam1111@gmail.com>
2026-08-10 17:22:00 +0530

    Enabled

--- mill_purchase/models/res_partner.py
+++ mill_purchase/models/res_partner.py
@@ -1,13 +1,9 @@
 from odoo import models, fields, api
 
 class ResPartner(models.Model):
-    _inherit = "res.partner"
-    _desc = "Mill Purchase Partner"
+    _inherit = 'res.partner'
 
-
-    # Field to select the Broker on the contract
     broker_id = fields.Many2one(
         'res.partner',
         string='Broker',
-        help="Broker responsible for coordinating this raw material supply run."
     )
